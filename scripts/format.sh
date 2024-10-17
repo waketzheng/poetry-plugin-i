a42788b6b576688c5d1efce971e897015ddc123c
Waket Zheng <waketzheng@gmail.com>
2024-10-17 20:24:30 +0800

    🔨 Update check and format script

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,6 +1,12 @@
 #!/bin/sh -e
 set -x
 
-[ -f ../pyproject.toml ] && cd ..
-poetry run ruff format .
-poetry run ruff check --fix .
+[ -f pyproject.toml ] || ([ -f ../pyproject.toml ] && cd ..)
+
+RUFF=ruff
+if ! [ -x "$(command -v ruff)" ]; then
+    poetry run ruff --version || poetry install
+    RUFF="poetry run ruff"
+fi
+$RUFF format .
+$RUFF check --fix --extend-select=I,B,SIM .
